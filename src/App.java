54a75358fa5b2acaef918d6a47a6ed65ba2fef52
Mateus Pitura <mateuspitura@gmail.com>
2023-03-26 18:06:14 -0300

    Entrada de valores com validação

--- src/App.java
+++ src/App.java
@@ -7,6 +7,20 @@ public class App {
         "Escolha uma opção", "Menu", JOptionPane.PLAIN_MESSAGE, null, 
         choices, choices[0]);
         
+        String entrada=(String)JOptionPane.showInputDialog(null,
+        "Insira um valor", "Input", JOptionPane.INFORMATION_MESSAGE);
         
+        double valor=0;
+        try{
+            valor=Double.parseDouble(entrada);
+        } catch(NumberFormatException ex){
+            JOptionPane.showMessageDialog(null, 
+            "Valor inválido, fim do programa", "Erro", JOptionPane.ERROR_MESSAGE);
+            throw new RuntimeException("Encerrando o programa");
+        }
+        System.out.println("[" + valor + "]");
+
+        JOptionPane.showMessageDialog(null, "Valor válido", 
+        "Acerto", JOptionPane.NO_OPTION);
     }
 }
